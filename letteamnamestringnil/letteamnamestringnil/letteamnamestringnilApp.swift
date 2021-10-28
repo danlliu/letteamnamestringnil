ae32e21f0116e1ef3435fc22bb7dbee8c363a900
Abigail Fox <abigailfireflyfox@gmail.com>
2021-10-28 12:27:04 -0400

    added xcode project

---
+++ letteamnamestringnil/letteamnamestringnil/letteamnamestringnilApp.swift
@@ -0,0 +1,20 @@
+//
+//  letteamnamestringnilApp.swift
+//  letteamnamestringnil
+//
+//  Created by Abigail Fox on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct letteamnamestringnilApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
