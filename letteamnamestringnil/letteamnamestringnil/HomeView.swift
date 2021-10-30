9b1f727d15228ad9882222f519ba5c17955e02d6
lexcov <alexcov@umich.edu>
2021-10-30 17:39:56 -0400

    Implemented home view

--- letteamnamestringnil/letteamnamestringnil/HomeView.swift
+++ letteamnamestringnil/letteamnamestringnil/HomeView.swift
@@ -7,21 +7,72 @@
 
 import SwiftUI
 
+//@main
 struct HomeView: View {
     
     var body: some View {
         NavigationView {
             VStack {
-                Button("Join Game", action: tempFunc)
-                Button("Create Game", action: tempFunc)
+                
+                Spacer()
+                
+                Text("TCP")
+                    .font(.largeTitle)
+//                    .padding(.bottom, 20.0)
+                
+                Spacer()
+                Spacer()
+
+                NavigationLink(
+                    destination: Text("Join"),
+                    label: {
+                        Text("Join Game")
+                    }
+                )
+                    .padding(.bottom, 12.0)
+                
+                NavigationLink(
+                    destination: Text("Create"),
+                    label: {
+                        Text("Create Game")
+                    }
+                )
+                
+                Spacer()
+                
             }
+            .buttonStyle(RedButton())
         }
     }
     
     func tempFunc() {
         return
     }
+    
+    /*
+     
+     Button("Join Game", action: tempFunc)
+         .padding(.bottom, 20.0)
+     
+     Button("Create Game", action: tempFunc)
+     
+     */
 }
+
+struct RedButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 180.0)
+            .padding()
+            .background(Color("redAccent"))
+            .foregroundColor(.white)
+            .clipShape(Rectangle())
+            .cornerRadius(3.0)
+            .font(.title)
+    }
+}
+
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
