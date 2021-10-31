98c300fb3055d53d0691db645d9f78b4f21646c4
Abigail Fox <abigailfireflyfox@gmail.com>
2021-10-31 15:27:07 -0400

    GameView template created with subview

--- letteamnamestringnil/letteamnamestringnil/GamesView.swift
+++ letteamnamestringnil/letteamnamestringnil/GamesView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct GamesView: View {
+    private var games = ["game1", "game2", "game3"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Games Joined")
+                .font(.title2)
+                .padding()
+            List {
+                ForEach(games, id: \.self) { game in
+                    GameInfoView(gameCode: game, username: "User")
+                }
+            }
+            .listStyle(.plain)
+        }
+    }
+}
+
+struct GameInfoView: View {
+    @State var gameCode: String
+    @State var username: String
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Text(gameCode)
+                Text(username)
+            }
+            Spacer()
+            Button(action: deleteGame) {
+                Image(systemName: "x.square")
+                    .foregroundColor(Color.red)
+            }
+        }
+    }
+    func deleteGame() {
+        //TODO: remove player from game in database
+        //if player created game, does whole game go away? backend problem
     }
 }
 
