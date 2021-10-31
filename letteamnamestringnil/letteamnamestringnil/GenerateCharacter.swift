313637b174e8942b21fcc2b3b1009e932abda2df
Abigail Fox <abigailfireflyfox@gmail.com>
2021-10-31 18:22:59 -0400

    added menus for class and alignment

--- letteamnamestringnil/letteamnamestringnil/GenerateCharacter.swift
+++ letteamnamestringnil/letteamnamestringnil/GenerateCharacter.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct GenerateCharacter: View {
+    @State private var name: String = ""
+    @State private var playerClass: String = "Select a class"
+    @State private var playerAlignment: String = "Select an alignment"
+    
+    private var classes = ["Aarakocra", "Bugbear", "Centaur", "Dragonborn", "Dwarf", "Elf", "Gensai", "Goblin", "Human", "Minotaur", "Orc", "Tortle",
+                            "Vedalken", "Warforged"]
+    private var alignments = ["Chaotic good", "Neutral good", "Lawful good", "Chaotic neutral", "Neutral neutral", "Lawful neutral", "Chaotic evil",
+                                "Neutral evil", "Lawful evil"]
     var body: some View {
-        Text("Generate Character")
+        VStack {
+            List {
+                HStack {
+                    Text("Name")
+                    TextField("Enter name here", text: $name)
+                }
+                HStack {
+                    Text("Class")
+                    VStack {
+                        Menu(playerClass) {
+                            ForEach(classes, id: \.self) { classOption in
+                                Button(classOption, action: {playerClass = classOption})
+                            }
+                        }
+                    }
+                }
+                HStack {
+                    Text("Alignment")
+                    VStack {
+                        Menu(playerAlignment) {
+                            ForEach(alignments, id: \.self) { alignmentOption in
+                                Button(alignmentOption, action: {playerAlignment = alignmentOption})
+                            }
+                        }
+                    }
+                }
+                HStack {
+                    
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem {
+                NavigationLink(destination: PlayerView()) {
+                    Text("Create")
+                }
+            }
+        }
+    }
+    
+    func generateRandom() {
+        //TODO: need database and ML
+        print("generating random character sheet...")
     }
 }
 
