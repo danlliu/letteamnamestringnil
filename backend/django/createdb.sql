82e7fdb37af02236551513a19c1705f17792b8f5
Ubuntu <ubuntu@ip-172-31-44-43.us-east-2.compute.internal>
2021-10-31 21:14:53 +0000

    Add Django app and createdb.sql script

---
+++ backend/django/createdb.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE djangodb;
+\connect djangodb
+
+CREATE USER letteamnamestringnil WITH PASSWORD 'h2gy13scmp0pskbnygcxj69zx3fb77ey';
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO letteamnamestringnil;
