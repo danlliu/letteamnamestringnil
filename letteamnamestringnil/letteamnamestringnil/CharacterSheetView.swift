b41ade9ec100bd2f0a666258a721611277813c40
lexcov <alexcov@umich.edu>
2021-11-01 13:34:51 -0400

    player views

--- letteamnamestringnil/letteamnamestringnil/CharacterSheetView.swift
+++ letteamnamestringnil/letteamnamestringnil/CharacterSheetView.swift
@@ -52,3 +52,86 @@ struct CharacterSheetView_Previews: PreviewProvider {
         CharacterSheetView()
     }
 }
+
+/*
+ 
+ // TODO: Delete
+ private var json: [String: Any] = [
+     "name": "Eayadon Falconmoon",
+     "class": "Fighter",
+     "level": 3,
+     "race": "Human",
+     "background": "Noble",
+     "alignment": 2,
+     "experiencePoints": 1416,
+     "abilityScores": [
+         "strength": 16,
+         "dexterity": 9,
+         "constitution": 15,
+         "intelligence": 11,
+         "wisdom": 13,
+         "charisma": 14,
+     ],
+     "inspiration": 0,
+     "proficiencyBonus": 2,
+     "savingThrows": [
+         "strength": true,
+         "dexterity": false,
+         "consitution": true,
+         "intelligence": false,
+         "widsom": false,
+         "charisma": false,
+     ],
+     "skills": [
+         "acrobatics": false,
+         "animalHandling": false,
+         "arcana": false,
+         "athletics": true,
+         "deception": false,
+         "history": true,
+         "insight": false,
+         "intimidation": false,
+         "investigation": false,
+         "medicine": false,
+         "nature": false,
+         "perception": true,
+         "performance": false,
+         "persuasion": true,
+         "religion": false,
+         "sleightOfHand": false,
+         "stealth": false,
+         "survival": false,
+     ],
+     "passiveWisdom": 13,
+     "otherProficienciesAndSkills": "Proficiencies: All armors, shields, simple weapons, martial weapons, playing cards\nLanguages: Common, Draconic, Dwarvish",
+     "stats": [
+         "armorClass": 17,
+         "initiative": -1,
+         "speed": 30,
+         "maxHP": 28,
+         "curHP": 28,
+         "tempHP": 12,
+         "hitDice": "1d10",
+         "deathSaveSuccess": 0,
+         "deathSaveFailure": 0,
+     ],
+     "attacks": "Javelin throw\n+5 ATK Bonus\nDamage:1d6 + 3 piercing",
+     "equipment": "Chain mail, greataxe, 3 javelins,        backpack, blanket, tinderbox, 2 days of rations, waterskin, set of fine clothes, signet ring, scroll of pedigree",
+     "personality": [
+         "traits": "My flattery makes those I talk to feel wonderful and important. Also, I don’t like to get dirty, and I won’t be caught dead in unsuitable accommodations.",
+         "ideals": "Responsiblity. It's the duty of a noble to protect the common people, not bully them.",
+         "bonds": "My greataxe is a family heirloom, and it's by far my most preious possession",
+         "flaws": "I have a hard time resisting the allure of wealth, especially gold. Wealth can help me restore my legacy.",
+     ],
+     "featuresAndTraits": "Second Wind. You have a limited well of stamina you can draw on to protect yourself from harm. You can use a bonus action to regain hit points equal to 1d10 + your fighter level. Once you use this feature, you must finish a short or long rest before you can use it again.",
+     "appearance": [],
+     "backstory": "Your family is no stranger to wealth, power, and privilege. I nthe glory days of Neverwinter, your parnts were the count and countess of Corlinn Hill, a large estate located in the hills northeast of the city. But MOunt Hotenow erupted thirty years ago, devastating Neverwinter and erasing Corlinn Hill from the map. Instaead of growing up on an estate, you were raised in a small but comfortable town housein in Waterdeep. As an adult, yo ustand to inherit a meaningless title and little else.",
+     "darkGifts": "",
+     "features": "",
+     "allies": "",
+     "treasure": "",
+     "spellcasting": [],
+     "spells": []
+ ]
+ 
+ */
--- letteamnamestringnil/letteamnamestringnil/PlayerDetailView.swift
+++ letteamnamestringnil/letteamnamestringnil/PlayerDetailView.swift
@@ -8,8 +8,99 @@
 import SwiftUI
 
 struct PlayerDetailView: View {
+    
+    @State private var partyCode = "E44W"
+    
+    @State private var characterName = "Eaydan Falconmoon"
+    @State private var className = "Fighter"
+    @State private var race = "Human"
+    @State private var level = 4
+    @State private var maxHP = 28
+    @State private var tempHP = 28
+    @State private var prof = "+4"
+    @State private var speed = "30"
+    @State private var initiative = "+5"
+    @State private var ac = "12"
+    @State private var alignment = 2
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Group {
+                Text("Party Code".uppercased())
+                    .font(.caption)
+                    .foregroundColor(.gray)
+                Text(partyCode)
+            }
+//            Spacer()
+            Group {
+                Text(characterName)
+                Text("\(className), \(race)")
+            }
+            Text("HP \(tempHP)/\(maxHP)")
+            Spacer()
+            Group {
+                VStack {
+                    HStack {
+                        Text("Level 10")
+                        Text("XP 1416")
+                    }
+                    HStack {
+                        
+                        VStack {
+                            Text("Proficiency")
+                            Text(prof)
+                        }
+                        VStack {
+                            Text("Speed")
+                            Text(speed)
+                        }
+                        VStack {
+                            Text("Initiative")
+                            Text(initiative)
+                        }
+                        VStack {
+                            Text("Armor Class")
+                            Text(ac)
+                        }
+                    }
+                }
+                List {
+                    HStack {
+                        VStack {
+                            Text("STR\t10")
+                            Text("DEX\t8")
+                            Text("CON\t6")
+                        }
+                        Spacer()
+                        VStack {
+                            Text("INT\t8")
+                            Text("WIS\t7")
+                            Text("CHR\t-5")
+                        }
+                    }
+                    HStack {
+                        Text("Add Skill")
+                        Spacer()
+                        Button(action: addSkill) {
+                            Image(systemName: "pencil")
+                                .foregroundColor(Color.red)
+                        }
+                    }
+                    HStack {
+                        Text("Add attack/spell")
+                        Spacer()
+                        Button(action: addSkill) {
+                            Image(systemName: "pencil")
+                                .foregroundColor(Color.red)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func addSkill() {
+        
     }
 }
 
